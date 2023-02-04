455f6d236aa7eb756645501a989cafe9a4ebb73f
samuelberhane <1samibrhane@gmail.com>
2023-02-04 13:52:10 +0300

    update backend and socket route to live deployed route.

--- frontend/src/utils/apiRoute.js
+++ frontend/src/utils/apiRoute.js
@@ -1,11 +1,11 @@
-const host = "http://localhost:8000";
+const host = process.env.REACT_APP_HOST;
 const postRoute = `${host}/api/posts`;
 const userRoute = `${host}/api/users`;
 const authRoute = `${host}/api/auth`;
 const messageRoute = `${host}/api/messages`;
 const uploadRoute = `${host}/api/upload`;
 const imageRoute = `${host}/images`;
-const socketRoute = "http://localhost:3001";
+const socketRoute = process.env.REACT_APP_SOCKET_ROUTE;
 
 export {
   host,
